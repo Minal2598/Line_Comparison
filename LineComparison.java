a0abcf0101870dab2cc61afad1fdb498814bcd16
Minal <patilminalp@gmail.com>
2021-06-16 14:16:01 +0530

    Added:compare two lines using equals method

--- LineComparison.java
+++ LineComparison.java
@@ -3,12 +3,12 @@ public class LineComparison{
 		
 int x1=1;
 int y1=1;
-int x2=2;
-int y2=2;
+int x2=5;
+int y2=5;
 int x3=3;
 int y3=3;
-int x4=4;
-int y4=4;
+int x4=10;
+int y4=10;
 		
 		System.out.println("the cordinates of line 1 are::" +"("+x1+","+y1+")"+"and"+"("+x2+","+y2+")");
 		double Length_line=Math.sqrt(((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1))); 
@@ -19,15 +19,8 @@ int y4=4;
 		double Length_line1=Math.sqrt(((x4-x3) * (x4-x3)) + ((y4-y3) * (y4-y3)));
 		System.out.println("Length of a line::"+Length_line1);
 		String str2 = Double.toString(Length_line1);
-		 if(str2.compareTo(str)>0)
-					 System.out.println( "Line2 is greater than Line1" );
-				 else if(str2.compareTo(str)==0)
-					 System.out.println( "Line2 is equals Line1");
-				 else 
-					 System.out.println("Line2 is smaller than Line1");
-					 
+		System.out.println(str.equals(str2));
+			            
 	}
 
 }
-
-
